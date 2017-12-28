bd7d6ee57c6b20d82f7ac64ed25722d929c647db
Sonali <soni_bb88@yahoo.co.in>
2017-12-28 10:58:13 +0530

    Login screen,Verify OTP & Cab request form dummy screen added with API integration.

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -28,6 +28,7 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+        alert("reader.onDeviceReady"); 
     },
 
     // Update DOM on a Received Event
